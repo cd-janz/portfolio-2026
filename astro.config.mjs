916c46cbe6a2f158700db894f38ea08a8ea7d8dd
cd-janz <cd.juanzuniga@gmail.com>
2026-01-05 21:46:56 -0500

    resume done by now

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-
-import mdx from '@astrojs/mdx';
-
 import vercel from '@astrojs/vercel';
-
 import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
